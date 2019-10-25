8cbcc5402fcf2cc35036d46c73a2f5c5e83de2a9
lcc <928857637@qq.com>
2019-10-25 09:21:02 +0800

    test

---
+++ demo/src/main/java/com/example/demo/controller/MyController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+/**
+ * @program: gamll
+ * @description:
+ * @author: 李朝朝
+ * @create: 2019-10-25 09:18
+ **/
+public class MyController {
+
+    public static void main(String[] args) {
+        System.out.println("啊哈哈");
+    }
+}
